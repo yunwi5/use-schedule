0d7a15007411a57c2134ac2400e3ec279f4c4f0d
Yunkeun <1009jyk@gmail.com>
2022-03-08 06:09:35 +1300

    TaskSearch backend and api implemented. Frontend user interface needs to be implemented.

--- pages/task-planner/search.tsx
+++ pages/task-planner/search.tsx
@@ -1,4 +1,4 @@
-// import type { NextPage } from 'next'
+import type { NextPage } from 'next'
 import { getSession } from "@auth0/nextjs-auth0";
 import { GetServerSideProps } from "next";
 import Head from "next/head";
@@ -13,7 +13,7 @@ interface Props {
 	searchWord: string;
 }
 
-const SearchPage: React.FC<Props> = (props) => {
+const SearchPage: NextPage<Props> = (props) => {
 	const { searchedTasks, searchWord } = props;
 	const [ plannerTasks, setPlannerTasks ] = useState<PlannerTask[]>([]);
 
@@ -28,10 +28,9 @@ const SearchPage: React.FC<Props> = (props) => {
 	return (
 		<Fragment>
 			<Head>
-				<title>Searched Planner Tasks for {}</title>
+				<title>Searched Planner Tasks for {searchWord}</title>
 				<meta name="description" content="User's search result for planner tasks" />
 			</Head>
-			<div>search</div>
 			<TaskSearch searchedTasks={plannerTasks} searchWord={searchWord} />
 		</Fragment>
 	);
@@ -51,11 +50,8 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 		};
 	}
 
-	const { q } = query;
-	let searchWord = q || "";
-	if (Array.isArray(searchWord)) {
-		searchWord = searchWord.join("");
-	}
+	const { q = '' } = query;
+    let searchWord = Array.isArray(q) ? q.join('') : q;
 
 	const searchedTasks: Task[] = await getSearchedTasks(searchWord, req.headers.cookie);
 
