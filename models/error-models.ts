b4f69f1d68cf4c1145360dd1c9a5ed5b84c04dec
Yunkeun <1009jyk@gmail.com>
2022-03-20 20:18:16 +1300

    Add custom NextError class

---
+++ models/error-models.ts
@@ -0,0 +1,11 @@
+export class NextError extends Error {
+	public name = "NextError";
+	public message: string;
+	public statusCode: number;
+
+	constructor (message: string, options: ErrorOptions, statusCode?: number) {
+		super(message, options);
+		this.message = message;
+		this.statusCode = statusCode || 404;
+	}
+}
