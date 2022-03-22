c6a53a06fe22f3945e11f60aff97d110f6fd370f
Yunkeun <1009jyk@gmail.com>
2022-03-23 09:38:29 +1300

    Template Planner component in construction.

--- components/templates/TemplatePlanner.tsx
+++ components/templates/TemplatePlanner.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 import { TemplateTask } from "../../models/template-models/TemplateTask";
 import { Template, TemplateFormObj } from "../../models/template-models/Template";
+import { plannerActions } from "../../store/redux/planner-slice";
+import { PlannerMode } from "../../models/planner-models/PlannerMode";
+
+function getTemplateWeekBeginning () {
+	// Template beginning time
+	// Mon Jan 01 1900 00:00:00 GMT+1130
+	const date = new Date(0, 0, 1);
+	return date;
+}
 
 interface Props {
 	onInvalidateTasks: () => void;
@@ -12,6 +22,18 @@ interface Props {
 
 const TemplatePlanner: React.FC<Props> = (props) => {
 	const { onInvalidateTasks, onMutateTemplate, templateTasks, template } = props;
+
+	const dispatch = useDispatch();
+	const templateWeekBeginning = getTemplateWeekBeginning();
+
+	dispatch(plannerActions.setBeginningPeriod(templateWeekBeginning.toString()));
+	// No need to navigate between different weeks and period, bc this is a template planner.
+
+	// Only runs on mount.
+	useEffect(() => {
+		dispatch(plannerActions.setPlannerMode(PlannerMode.TEMPLATE));
+	}, []);
+
 	return <div>TemplatePlanner</div>;
 };
 
