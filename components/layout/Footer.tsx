fd2499b8c073286f7d25dcebd0d20be8242fa80d
Yunkeun <1009jyk@gmail.com>
2022-03-31 23:57:19 +1300

    Brand icons added.

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,7 +1,18 @@
+import { faFacebook, faGithub, faGithubAlt, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 const Footer: React.FC = () => {
-	return <footer>Footer</footer>;
+	return (
+		<footer className=''>
+			<p>&copy; 2022 JYK Inc. All rights reserved.</p>
+			<div>
+				<FontAwesomeIcon icon={faGithub as any} />
+				<FontAwesomeIcon icon={faFacebook as any} />
+				<FontAwesomeIcon icon={faLinkedin as any} />
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
