41c15ef92aa8958259ae88684396b0f268d74b4d
Yunkeun <1009jyk@gmail.com>
2022-04-01 09:49:10 +1300

    Footer completed.

--- utilities/mongodb-util/mongodb-constant.ts
+++ utilities/mongodb-util/mongodb-constant.ts
@@ -1,15 +1,10 @@
 export enum Collection {
-	WEEKLY_TASKS = "weekly-tasks",
-	MONTLY_TASKS = "montly-tasks",
-	YEARLY_TASKS = "yearly-tasks",
-	TEMPLATE_TASKS = "template-tasks"
+	WEEKLY_TASKS = 'weekly-tasks',
+	MONTLY_TASKS = 'montly-tasks',
+	YEARLY_TASKS = 'yearly-tasks',
+	TEMPLATE_TASKS = 'template-tasks',
 }
 
-// export enum SubCollection {
-// 	PLANNER_SUBTASKS = "sub-tasks",
-// 	TEMPLATE_SUBTAKS = "template-sub-tasks"
-// }
+export const SubTaskCollection = 'sub-tasks';
 
-export const SubTaskCollection = "sub-tasks";
-
-export const TemplateCollection = "templates";
+export const TemplateCollection = 'templates';
